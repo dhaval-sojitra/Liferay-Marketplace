1594917aa6ae003483a9b540b2c456d712cd4bc5
dhaval-sojitra <dhavalsojitra875@gmail.com>
2025-05-16 12:24:10 +0530

    Added

---
+++ eclipse-workspace/liferay-marketplace/modules/orphen-fragment-cleaner-web/src/main/java/com/ignek/marketplace/orphen/fragment/cleaner/constants/OrphenFragmentCleanerWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.ignek.marketplace.orphen.fragment.cleaner.constants;
+
+/**
+ * @author dell
+ */
+public class OrphenFragmentCleanerWebPortletKeys {
+
+	public static final String ORPHEN_FRAGMENT_CLEANER_WEB_PORTLET_KEY =
+		"com_liferay_marketplace_orphen_fragment_cleaner_OrphenFragmentCleanerWebPortlet";
+
+}
